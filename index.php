9763c87e55aefb6ba4ecbcc9debebcb6cd039e48
n11kkou <nurgalievakamila7@gmail.com>
2021-02-08 21:59:37 +0600

    add names to all types of arrays

--- index.php
+++ index.php
@@ -10,21 +10,25 @@
 
 <body>
     <?php
+    echo '<h3>Indexed Array</h3>';
     $ind_arr = array("Kokshetau", "Karaganda", "Astana");
     echo '<pre>';
     print_r($ind_arr);
     echo '</pre>';
 
+    echo '<h3>Array With My Indeces</h3>';
     $key_arr = [3 => 'Kokshetau', 9 => 'Karaganda', 1 => 'Astana'];
     echo '<pre>';
     print_r($key_arr);
     echo '</pre>';
 
+    echo '<h3>Associative Arrays</h3>';
     $assc_arr = array(19 => 'Kamila', 16 => 'Anais', 25 => 'Nursultan', 18 => 'Dilnaz');
     echo '<pre>';
     print_r($assc_arr);
     echo '</pre>';
 
+    echo '<h3>Multidimensional Arrays</h3>';
     $cars = array(
         array("Volvo", 22, 18),
         array("BMW", 15, 13),
