fee130084486ebdb4a523dfa714d4a4f908e1184
n11kkou <nurgalievakamila7@gmail.com>
2021-02-08 20:04:57 +0600

    create indexed array

--- index.php
+++ index.php
@@ -1,12 +1,25 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>PHP</title>
 </head>
+
 <body>
-    
+    <?php
+    $ind_arr = array("Kokshetau", "Karaganda", "Astana");
+    echo '<pre>';
+    print_r($ind_arr);
+    echo '</pre>'
+
+
+
+    ?>
+
+
 </body>
+
 </html>
